28e5c0f99cf06d277414775de87d13cfba7b0507
Shannon Webster <shanoonwebbie@gmail.com>
2024-05-26 15:18:04 -0700

    inital commit

---
+++ dog-years.js
@@ -0,0 +1,11 @@
+let myAge = 31;
+let earlyYears = 2;
+earlyYears *= 10;
+// the first 2 years of dog years equal 10 human years
+let laterYears = myAge - 2
+// after the first 2 years dog 1 dog year equals 4 human years
+laterYears *= 4
+// calculating the result of my age in dog years
+const myAgeInDogYears = earlyYears + laterYears
+let myName = "Shannon".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years.`)
